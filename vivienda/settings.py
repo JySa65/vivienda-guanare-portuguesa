55a095e52f72041cb57be554897de3bdf10de380
JySa65 <r.a.s.g65@gmail.com>
2018-10-07 05:52:32 -0400

    heroku 3

--- vivienda/settings.py
+++ vivienda/settings.py
@@ -10,23 +10,16 @@ MIDDLEWARE += [
      'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite',
-#         'NAME': 'djangogirls',
-#         'USER': 'name',
-#         'PASSWORD': '',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vivienda',
+        'USER': 'name',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
